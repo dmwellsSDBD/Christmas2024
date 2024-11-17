8e252e3dcdb58bea64b9ddcda37d88f760eb4db7
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2024-11-17 13:11:23 +0000

    Init Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+        'red': '#800000',
+        'green': '#4A817B',
+        'white': '#ffffff',
+        'dkRed': '#560000',
+        'dkGreen': '#293938',
+    },
+    minHeight: {
+      'section': '25rem',
+      'header': '40rem',
+    },
+    extend: {
+      fontFamily: {
+        "xmas1": ["Mountains of Christmas", "serif"],
+        "xmas2": ["Caveat Brush", "cursive"],
+        "xmas3": ["Delius", "sans-serif"],
+        "xmas4": ["Henny Penny", "system-ui", "serif"]
+      },
+      backgroundImage: {
+        'hero-home': "url('/src/img/hero-index.jpg')",
+      },
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  plugins: [
+    require('tailwind-fontawesome')
+  ],
+}
